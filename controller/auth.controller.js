fdce67cff3bc4555842aa0b7555302b707cb49d5
Rahul <rahulmahato0852@gmail.com>
2024-07-31 12:28:15 +0530

    user schema created

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,3 +1,21 @@
-const express = require('express')
+const asyncHandler = require('express-async-handler');
+const { ValidationNew } = require('../utils/validation');
 
 
+exports.adminLogin = asyncHandler(async (req, res) => {
+
+    const { email, password } = req.body
+
+    const x = ValidationNew(res, [
+        { keyname: "email", value: email, validations: [{ except: false, key: "isEmpty" }, { except: true, key: "isEmail" }] },
+        { keyname: "password", value: password, validations: [{ except: false, key: "isEmpty" }, { except: true, key: "isStrongPassword" }] }
+    ])
+    if (x) {
+        return;
+    }
+
+
+
+
+
+})
---
+++ models/User.js
@@ -0,0 +1,26 @@
+const { default: mongoose } = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    mobile: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+}, {
+    timestamps: true
+})
+
+
+module.exports = mongoose.model("user", userSchema)
+
